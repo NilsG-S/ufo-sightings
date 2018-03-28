a73d8a4896735f24497f20d5c3fb59e2fd285e46
Orion Koepke <orion.koepke@ttu.edu>
2018-03-28 15:20:05 -0500

    Added code to BDManager for creating databases and checking if they exist, creating tables, and loading csv files into tables.

--- database_creation.py
+++ database_creation.py
@@ -1,7 +1,9 @@
 import atexit
 import mysql.connector
 import mysql.connector.errorcode
+import os
 
+from dotenv import load_dotenv
 
 class DBManager:
 
@@ -37,6 +39,12 @@ class DBManager:
     def create_db(self, database_name):
         sql = "CREATE DATABASE %s;" % database_name
         self.cursor.execute(sql)
+        self.reset_cursor()
+
+    def delete_database(self, database_name):
+        sql = "DROP DATABASE %s;" % database_name
+        self.cursor.execute(sql)
+        self.reset_cursor()
 
     def set_database(self, database_name):
         try:
@@ -51,14 +59,65 @@ class DBManager:
         else:
             self.reset_cursor()
 
-    def create_table(self, table_sql):
-        self.cursor.execute(table_sql)
+    def database_exists(self, database_name):
+        sql = "SHOW DATABASES LIKE '%s';" % database_name
+        self.cursor.execute(sql)
+        answer = len(self.cursor.fetchall()) == 1
+
+        self.reset_cursor()
+
+        return answer
+
+    def create_table(self, table_name, table_schema):
+        sql = "CREATE TABLE %s %s;" % (table_name, table_schema)
+        self.cursor.execute(sql)
 
     def insert_csv_into_table(self, table_name, csv_file_path):
-        sql = "LOAD DATA INFILE %s INTO TABLE %s;" % (csv_file_path, table_name)
+        sql = "LOAD DATA LOCAL INFILE '%s' INTO TABLE %s " \
+              "FIELDS TERMINATED BY ','" \
+              "IGNORE 1 LINES;" % (csv_file_path, table_name)
         self.cursor.execute(sql)
         self.db_connection.commit()
 
     def close_database(self):
         self.cursor.close()
         self.db_connection.close()
+
+
+# Load the environment file for environment variable.
+load_dotenv("./.env")
+
+# Get the database username and password from the .env file.
+db_user_name = os.getenv("DB_USER_NAME")
+db_password = os.getenv("DB_PASSWORD")
+
+db_name = "ufo"
+
+# Create a database manager to start editing the database.
+db_manager = DBManager(user_name=db_user_name, password=db_password)
+
+# If a database named that vale stored in db_name already exists, delete it.
+if db_manager.database_exists(db_name):
+    db_manager.delete_database(db_name)
+
+# Create a database named the value stored in db_name and set the working database to this database.
+db_manager.create_db(db_name)
+db_manager.set_database(db_name)
+
+# Create airportData table and put the airportData.csv data into the table.
+airport_data_schema = "(" \
+                      "id int(8) NOT NULL," \
+                      "airport_code varchar(40)," \
+                      "type varchar(40)," \
+                      "name varchar(40)," \
+                      "latitude_deg double(9, 6)," \
+                      "longitude_deg double(9, 6)," \
+                      "elevation int(6)," \
+                      "country varchar(40)," \
+                      "state varchar(40)," \
+                      "city varchar(40)," \
+                      "zip_code int(5)," \
+                      "PRIMARY KEY (id)" \
+                      ")"
+db_manager.create_table("airportData", airport_data_schema)
+db_manager.insert_csv_into_table("airportData", "/Users/okoepke/Desktop/ufo-sightings/CleanData/AirportData.csv")
