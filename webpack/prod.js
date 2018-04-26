ed85b26c653d60df3c277449d52e09ef8b378ce4
NilsG-S <nils.griswold.steiner@gmail.com>
2018-04-26 15:02:43 -0500

    Finished further basic setup

---
+++ webpack/prod.js
@@ -0,0 +1,15 @@
+const webpack = require('webpack');
+const webpackMerge = require('webpack-merge');
+const path = require('path');
+
+const base = require('./base.js');
+
+const prod = webpackMerge(base, {
+  plugins: [
+    new webpack.DefinePlugin({
+      ENV: JSON.stringify('prod'),
+    }),
+  ],
+});
+
+module.exports = prod;
