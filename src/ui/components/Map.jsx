de173127e1db567c5c088c06b06ca40b3b1d0ca9
NilsG-S <nils.griswold.steiner@gmail.com>
2018-05-01 02:56:29 -0500

    VICTORY LIES BEFORE USgit status!

--- src/ui/components/Map.jsx
+++ src/ui/components/Map.jsx
@@ -42,27 +42,38 @@ class Map extends React.Component {
       ready: false,
     };
     this.mapRef = React.createRef();
+    this.map = null;
   }
 
   componentDidMount() {
-    const states = loadJSON('states.json')
-      .then((res) => { data.state.mil = Object.entries(res); })
+    this.map = new google.maps.Map(this.mapRef.current, {
+      zoom: 4,
+      center: { lat: 33.584466, lng: -101.874670 },
+    });
+
+    const states = loadJSON('states.geojson')
+      .then((res) => { data.states = res; })
+      .catch((err) => { console.log(err); });
+
+    const counties = loadJSON('counties.geojson')
+      .then((res) => { data.counties = res; })
       .catch((err) => { console.log(err); });
 
     const all = loadJSON('all.json')
-      .then((res) => { data.none.all = Object.entries(res); })
+      .then((res) => { data.none.all = res; })
       .catch((err) => { console.log(err); });
 
     const airports = loadJSON('airports.json')
-      .then((res) => { data.none.air = Object.entries(res); })
+      .then((res) => { data.none.air = res; })
       .catch((err) => { console.log(err); });
 
     const airportsState = loadJSON('airportsState.json')
-      .then((res) => { data.state.air = Object.entries(res); })
+      .then((res) => { data.state.air = res; })
       .catch((err) => { console.log(err); });
 
     Promise.all([
       states,
+      counties,
       all,
       airports,
       airportsState,
@@ -87,14 +98,13 @@ class Map extends React.Component {
   }
 
   componentDidUpdate() {
-    const map = new google.maps.Map(this.mapRef.current, {
-      zoom: 4,
-      center: { lat: 33.584466, lng: -101.874670 },
-    });
-
-    // if (this.props.geoChecked === 'none') {
-    // } else {
-    // }
+    if (this.props.geoChecked === 'none') {
+      this.map.data.addGeoJson(data.states);
+    } else {
+      this.map.data.forEach((feature) => {
+        this.map.data.remove(feature);
+      });
+    }
   }
 
   render() {
