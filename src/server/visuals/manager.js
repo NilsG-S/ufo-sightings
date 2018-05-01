bb4e4dd2bd156797b5ad769e2a427a1da809a3b5
NilsG-S <nils.griswold.steiner@gmail.com>
2018-05-01 04:57:46 -0500

    Finished all data and military data

--- src/server/visuals/manager.js
+++ src/server/visuals/manager.js
@@ -9,6 +9,18 @@ async function all() {
   return conn.exec(allSQL, []);
 }
 
+const allStateSQL = `
+SELECT A.state, COUNT(*) AS num
+ FROM ufosightings U
+ INNER JOIN addresses A
+ ON U.latitude_deg = A.latitude_deg AND U.longitude_deg = A.longitude_deg
+ GROUP BY A.state;
+`;
+
+async function allState() {
+  return conn.exec(allStateSQL, []);
+}
+
 const airportsSQL = `
 SELECT DISTINCT U.latitude_deg AS lat, U.longitude_deg AS lng
  FROM ufosightings U
@@ -55,8 +67,57 @@ async function airportsState() {
   return out;
 }
 
+const militarySQL = `
+SELECT DISTINCT U.latitude_deg AS lat, U.longitude_deg AS lng
+ FROM ufosightings U
+ INNER JOIN addresses A
+ ON U.latitude_deg = A.latitude_deg AND U.longitude_deg = A.longitude_deg
+ INNER JOIN
+ (
+   SELECT DISTINCT county, state
+   FROM militarybases M
+   INNER JOIN addresses A
+   ON M.latitude_deg = A.latitude_deg AND M.longitude_deg = A.longitude_deg
+ ) M
+ ON A.state = M.state AND A.county = M.county;
+`;
+
+async function military() {
+  return conn.exec(militarySQL, []);
+}
+
+const militaryStateSQL = `
+SELECT A.state, COUNT(*) AS num
+ FROM ufosightings U
+ INNER JOIN addresses A
+ ON U.latitude_deg = A.latitude_deg AND U.longitude_deg = A.longitude_deg
+ INNER JOIN
+ (
+   SELECT DISTINCT state
+   FROM militarybases M
+   INNER JOIN addresses A
+   ON M.latitude_deg = A.latitude_deg AND M.longitude_deg = A.longitude_deg
+ ) M
+ ON A.state = M.state
+ GROUP BY A.state;
+`;
+
+async function militaryState() {
+  const res = await conn.exec(militaryStateSQL, []);
+  const out = {};
+
+  res.forEach((row) => {
+    out[row.state] = row.num;
+  });
+
+  return out;
+}
+
 module.exports = {
   all,
+  allState,
   airports,
   airportsState,
+  military,
+  militaryState,
 };
--- src/server/visuals/router.js
+++ src/server/visuals/router.js
@@ -14,6 +14,16 @@ router.get('/none/all', (req, res) => {
     });
 });
 
+router.get('/state/all', (req, res) => {
+  manager.allState()
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(400).json({ message: err.toString() }).end();
+    });
+});
+
 router.get('/none/airports', (req, res) => {
   manager.airports()
     .then((data) => {
@@ -26,6 +36,26 @@ router.get('/none/airports', (req, res) => {
 
 router.get('/state/airports', (req, res) => {
   manager.airportsState()
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(400).json({ message: err.toString() }).end();
+    });
+});
+
+router.get('/none/military', (req, res) => {
+  manager.military()
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(400).json({ message: err.toString() }).end();
+    });
+});
+
+router.get('/state/military', (req, res) => {
+  manager.militaryState()
     .then((data) => {
       res.status(200).json(data);
     })
